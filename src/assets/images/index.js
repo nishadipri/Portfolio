28898ad41367b064af755c0379a6a4a6c5093af2
Nishadi Samarathunga <nishadi@nishadis-macbook-pro-2.home>
2024-03-21 14:04:51 +0100

    added some files

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+// import all images from image folder
+
+// export files
+
+export  {
+    image1,
+    image2,
+    image3,
+    image4
+};
---
+++ src/constants/index.js
@@ -0,0 +1,40 @@
+// you can change this according to your needs
+
+
+export const navLinksdata = [
+    
+    {
+        _id: 1001,
+        title: "Home",
+        link: "home"
+    },
+    {
+        _id: 1002,
+        title: "Features",
+        link: "features"
+    },
+    {
+        _id: 1003,
+        title: "Projects",
+        link: "projects"
+    },
+
+    {
+        _id: 1004,
+        title: "Resume",
+        link: "resume"
+    },
+    {
+        _id: 1005,
+        title: "Testimonial",
+        link: "testimonials"
+    },
+
+
+    {
+        _id: 1006,
+        title: "Contact",
+        link: "contact"
+    },
+
+]
